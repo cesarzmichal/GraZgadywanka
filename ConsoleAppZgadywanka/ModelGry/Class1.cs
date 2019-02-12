4485a69856b048e573a74207497ed9e79dce42e3
cesarzmichal <47394133+cesarzmichal@users.noreply.github.com>
2019-02-12 20:19:00 +0100

    wersja 1.1
    gra obiektowo

---
+++ ConsoleAppZgadywanka/ModelGry/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ModelGry
+{
+    public class Gra    
+    {
+        int wylosowana;
+        int zakresOd;
+        int zakresDo;
+
+        //historia
+
+        //konstruktor domyślny
+        public Gra()
+        {
+            zakresOd = 1;
+            zakresDo = 100;
+            wylosowana = (new Random()).Next(zakresOd, zakresDo + 1);
+            // historia = pusta;
+        
+        }
+
+        // konstruktor ogólny
+        public Gra (int min, int max)
+        {
+            zakresOd = min;
+            zakresDo = max;
+            wylosowana = (new Random()).Next(zakresOd, zakresDo + 1);
+            // historia = pusta;
+        }
+    }
+}
--- ConsoleAppZgadywanka/Program.cs
+++ ConsoleAppZgadywanka/Program.cs
@@ -15,12 +15,66 @@ namespace ConsoleAppZgadywanka
     /// </summary>
     class Program
     {
-        static void Main(string[] args)
+        static int Losuj(int min = 1, int max = 100)
         {
-            // 1. Komputer losuje liczbę od 1 do 100
-            int wylosowana = (new Random()).Next(1, 101);
-            Console.WriteLine(wylosowana);
-            Console.WriteLine("Wylosowałem liczbę od 1 do 100 \n odgadnij ją");
+            if (min > max)
+                {
+                min = 1;
+                max = 101;
+            }
+            Console.WriteLine($"Losuję {min} do {max} \n odgadnij ją");
+            return (new Random().Next(min, max + 1));
+        }
+
+        static int Wczytajliczbe(string prompt)
+        {
+            bool poprawnie = false;
+            int liczba = 0;
+
+            while (!poprawnie)
+            {
+                string napis = "";
+                Console.Write(prompt);
+                try
+                {
+                    napis = Console.ReadLine();
+                    if (napis.ToUpper() == "X")
+                    {
+                        Console.WriteLine("Aplikacja przerwana na polecenie uzytkownika");
+                        Environment.Exit(1);
+                    }
+
+                    liczba = Convert.ToInt32(napis);
+                    poprawnie = true;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("podałeś zbyt dużą liczbę - spróbuj jeszcze raz");
+                    continue;
+                    // throw new OverflowException(); //podaj dalej
+                }
+
+                catch (FormatException)
+                {
+                    Console.WriteLine("nie podałeś poprawnej liczby - koniec programu");
+                    continue;
+                }
+            }
+            return liczba;
+        }
+        static void Main()
+        {
+            // 1. Komputer losuje liczbę
+            Console.WriteLine("podaj zakres losowania liczby do odgadnięcia");
+
+            int min = Wczytajliczbe("podaj wartość od:");
+            int max = Wczytajliczbe("podaj wartość do:");
+            
+            int wylosowana = (new Random()).Next(min, max);
+
+#if DEBUG      
+            Console.WriteLine("Tajne " + wylosowana);
+#endif           
 
             Stopwatch stopper = Stopwatch.StartNew();
             int licznik = 0;
@@ -30,26 +84,9 @@ namespace ConsoleAppZgadywanka
             //Dopóki nie trafiono
             while (!trafiono)
             {
-                int propozycja = 0;
-
                 // 2. Człowiek podaje propozycję
-                Console.Write("Podaj propozycję: ");
+                int propozycja = Wczytajliczbe("Podaj swoją propozycję: ");
 
-                try
-                {
-                   propozycja = Convert.ToInt32(Console.ReadLine());
-                }
-                catch(OverflowException)
-                {
-                    Console.WriteLine(  "podałeś zbyt dużą liczbę - koniec programu");
-                    break;
-                }
-
-                catch(FormatException)
-                {
-                    Console.WriteLine("pnie podałeś poprawnej liczby - koniec programu");
-                    continue;
-                }
 
                 // 3. Komputer ocenia propozycję
                 if (propozycja < wylosowana)
